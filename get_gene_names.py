dea0e08c7cf519ac3b3f7f3771ab18f9de231601
chiangtw <chiangtw112358@gmail.com>
2023-06-01 05:46:31 +0800

    "na_value" for "get_gene_names.py"

--- get_gene_names.py
+++ get_gene_names.py
@@ -14,7 +14,7 @@ def read_region_file(region_file_object):
         yield chrm, pos1, pos2, strand, data
 
 
-def get_gene_names(chrm, pos1, pos2, strand, anno_db):
+def get_gene_names(chrm, pos1, pos2, strand, anno_db, na_value):
     if strand == "+":
         donor_pos, acceptor_pos = pos2, pos1
     elif strand == "-":
@@ -23,33 +23,39 @@ def get_gene_names(chrm, pos1, pos2, strand, anno_db):
     donor = anno_db.get_donor_site(chrm, donor_pos, strand)
     acceptor = anno_db.get_acceptor_site(chrm, acceptor_pos, strand)
 
-    donor_genes = set()
+    donor_gene_set = set()
     if donor:
         for exon in donor.exons:
             for transcript in exon.transcript:
-                donor_genes.add(transcript.gene.gene_symbol)
+                donor_gene_set.add(transcript.gene.gene_symbol)
+        donor_genes = ','.join(sorted(donor_gene_set))
+    else:
+        donor_genes = na_value
 
-    acceptor_genes = set()
+    acceptor_gene_set = set()
     if acceptor:
         for exon in acceptor.exons:
             for transcript in exon.transcript:
-                acceptor_genes.add(transcript.gene.gene_symbol)
+                acceptor_gene_set.add(transcript.gene.gene_symbol)
+        acceptor_genes = ','.join(sorted(acceptor_gene_set))
+    else:
+        acceptor_genes = na_value
 
-    if (len(donor_genes) > 0) and (len(acceptor_genes) > 0):
-        common_genes = donor_genes & acceptor_genes
+    if (len(donor_gene_set) > 0) and (len(acceptor_gene_set) > 0):
+        common_genes = donor_gene_set & acceptor_gene_set
         if len(common_genes) > 0:
             intragenic = 1
         else:
             intragenic = 0
     else:
-        intragenic = ''
+        intragenic = na_value
 
     if strand == "+":
-        pos1_genes = ','.join(sorted(acceptor_genes))
-        pos2_genes = ','.join(sorted(donor_genes))
+        pos1_genes = acceptor_genes
+        pos2_genes = donor_genes
     elif strand == "-":
-        pos1_genes = ','.join(sorted(donor_genes))
-        pos2_genes = ','.join(sorted(acceptor_genes))
+        pos1_genes = donor_genes
+        pos2_genes = acceptor_genes
 
     return pos1_genes, pos2_genes, intragenic
 
@@ -62,6 +68,11 @@ def create_parser():
         type=argparse.FileType('r'),
         help="4-columns TSV file: (chrm, pos1, pos2, strand)"
     )
+    parser.add_argument(
+        '--na_value',
+        default='NA',
+        help='Placeholder for na values.'
+    )
 
     return parser
 
@@ -73,5 +84,5 @@ if __name__ == "__main__":
     anno_db = Annotation(args.anno_db)
 
     for chrm, pos1, pos2, strand, data in read_region_file(args.region_file):
-        pos1_genes, pos2_genes, intragenic = get_gene_names(chrm, pos1, pos2, strand, anno_db)
+        pos1_genes, pos2_genes, intragenic = get_gene_names(chrm, pos1, pos2, strand, anno_db, args.na_value)
         print(*data, pos1_genes, pos2_genes, intragenic, sep='\t', flush=True)
